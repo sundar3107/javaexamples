b3ca374ba58b68bf94bd6d8ac4929b2d0ee5eedb
Sundar.M.M <mohanv39613@gmail.com>
2026-03-24 12:05:30 +0530

    VERSION 2

--- src/gitconfirmation.java
+++ src/gitconfirmation.java
@@ -1,5 +1,5 @@
 public class gitconfirmation {
-    public static void main(String args[]){
-        System.out.println("confirm git");
+    public static void main(String[] args){
+        System.out.println("corm git version 44772");
     }
 }
--- src/j1.java
+++ src/j1.java
@@ -1,5 +1,5 @@
 public class j1 {
     public static void main (String[] args){
-        System.out.println("hello world");
+        System.out.println("hello world iam sundar 222");
     }
 }
