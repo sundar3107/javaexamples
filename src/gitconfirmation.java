3ccb621bd9cd59637dbe14b0fe6af78a09eb71c6
Sundar.M.M <mohanv39613@gmail.com>
2026-03-25 12:51:13 +0530

    version online

--- src/gitconfirmation.java
+++ src/gitconfirmation.java
@@ -1,5 +1,5 @@
 public class gitconfirmation {
     public static void main(String args[]){
-        System.out.println("confirm git");
+        System.out.println("confirm git online checking");
     }
 }
